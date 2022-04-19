2c6ec07508cf85290c7d529e4e2560dba6487c7f
Kofi Quansah <kquansah@ucsc.edu>
2022-04-19 12:35:14 -0700

    first commit

---
+++ src/scenes/Menu.js
@@ -0,0 +1,49 @@
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("menu");
+    }
+    preload() {
+        
+    }
+    create(){
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#57e1ff',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+        this.bg = this.add.tileSprite(0,0, game.config.width, game.config.height, 'menubackground').setOrigin(0, 0);
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
+        
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    }
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+            // easy mode
+            game.settings = {
+              spaceshipSpeed: 3,
+              gameTimer: 60000    
+            }
+            this.sound.play('sfx_select');
+            this.scene.start('play');    
+        }
+        if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+            // hard mode
+            game.settings = {
+                spaceshipSpeed: 4,
+                gameTimer: 45000    
+            }
+            this.sound.play('sfx_select');
+            this.scene.start('play');    
+        }
+    }
+}
