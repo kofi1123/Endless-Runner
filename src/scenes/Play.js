8d0d27262227c00e0fe4a4e031bfb213e3aea171
Kofi Quansah <kquansah@ucsc.edu>
2022-04-30 03:25:02 -0700

    Added indicator before train arrives

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,7 +11,12 @@ class Play extends Phaser.Scene {
         this.load.image('blueMiddleObj', './assets/images/blueMiddleObj.png');
         this.load.image('yellowEndObj', './assets/images/yellowEndObj.png');
         this.load.image('yellowMiddleObj', './assets/images/yellowMiddleObj.png');
-        this.load.image('coin', './assets/images/spr_coinPlaceholder.png');
+        this.load.spritesheet('redTrainIndicatorV', './assets/images/redTrainIndicatorV.png', {frameWidth: 3, frameHeight: 100, startFrame: 0, endFrame: 9});
+        this.load.spritesheet('redTrainIndicatorH', './assets/images/redTrainIndicatorH.png', {frameWidth: 100, frameHeight: 3, startFrame: 0, endFrame: 9});
+        this.load.spritesheet('blueTrainIndicatorV', './assets/images/blueTrainIndicatorV.png', {frameWidth: 3, frameHeight: 100, startFrame: 0, endFrame: 9});
+        this.load.spritesheet('blueTrainIndicatorH', './assets/images/blueTrainIndicatorH.png', {frameWidth: 100, frameHeight: 3, startFrame: 0, endFrame: 9});
+        this.load.spritesheet('yellowTrainIndicatorV', './assets/images/yellowTrainIndicatorV.png', {frameWidth: 3, frameHeight: 100, startFrame: 0, endFrame: 9});
+        this.load.spritesheet('yellowTrainIndicatorH', './assets/images/yellowTrainIndicatorH.png', {frameWidth: 100, frameHeight: 3, startFrame: 0, endFrame: 9});
         this.load.spritesheet('coinrotate', './assets/images/CoinSpriteSheet.png', {frameWidth: 100, frameHeight: 100, startFrame: 0, endFrame: 3});
         this.load.audio('sfx_music', './assets/sound/sfx_music.mp3');
         this.load.audio('sfx_coin', './assets/sound/sfx_coin.mp3');
@@ -41,6 +46,48 @@ class Play extends Phaser.Scene {
             repeat: -1
         });
 
+        this.anims.create({
+            key: 'redTrainIndicatorV',
+            frames: this.anims.generateFrameNumbers('redTrainIndicatorV', {frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'redTrainIndicatorH',
+            frames: this.anims.generateFrameNumbers('redTrainIndicatorH', {frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'blueTrainIndicatorV',
+            frames: this.anims.generateFrameNumbers('blueTrainIndicatorV', {frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'blueTrainIndicatorH',
+            frames: this.anims.generateFrameNumbers('blueTrainIndicatorH', {frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'yellowTrainIndicatorV',
+            frames: this.anims.generateFrameNumbers('yellowTrainIndicatorV', {frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'yellowTrainIndicatorH',
+            frames: this.anims.generateFrameNumbers('yellowTrainIndicatorH', {frames: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]}),
+            frameRate: 10,
+            repeat: -1
+        });
+
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
@@ -65,13 +112,16 @@ class Play extends Phaser.Scene {
         this.bgSound = this.sound.add('sfx_music', {loop: true});
         this.bgSound.play();
         
-        this.trainColors = [['redEndObj', 'redMiddleObj'], ['yellowEndObj', 'yellowMiddleObj'], ['blueEndObj', 'blueMiddleObj']];
+        this.trainColors = [['redEndObj', 'redMiddleObj', 'redTrainIndicatorV', 'redTrainIndicatorH'],
+            ['yellowEndObj', 'yellowMiddleObj', 'yellowTrainIndicatorV', 'yellowTrainIndicatorH'],
+            ['blueEndObj', 'blueMiddleObj', 'blueTrainIndicatorV', 'blueTrainIndicatorH']];
         //Systems for timer
         this.currentTime = 0;
-        this.intervalTIme = 120;
+        this.intervalTime = 120;
         this.trainSpeed = 2;
-        this.time = 0;
-        this.timeRight = this.add.text(game.config.width - (borderPadding*2), borderPadding * 0.5,"Score: " + Math.floor(this.time / 1000), this.scoreConfig).setOrigin(0.5);
+        this.timer = 0;
+        this.timeRight = this.add.text(game.config.width - (borderPadding*2), borderPadding * 0.5,"Score: " + Math.floor(this.timer / 1000), this.scoreConfig).setOrigin(0.5);
+        this.indicateQueue = new Queue();
     }
     
     update(time, delta) {
@@ -79,8 +129,8 @@ class Play extends Phaser.Scene {
             this.player.update();
             let increment = 60/(1000/delta);
             let rand = Math.random() * 1.5;
-            this.timeRight.text = "Score: " + Math.floor(this.time / 10);
-            this.time += delta;
+            this.timeRight.text = "Score: " + Math.floor(this.timer / 10);
+            this.timer += delta;
             //Update Trains
             let trainArr = this.trains.getChildren();
             for(let i = 0; i < trainArr.length; i++){
@@ -97,13 +147,14 @@ class Play extends Phaser.Scene {
             this.checkCollisionCoin(this.coin);
             //Spawns new trains
             this.currentTime += increment;
-            if(this.currentTime >= this.intervalTIme){
+            if(this.currentTime >= this.intervalTime){
 
                 let dir = Phaser.Math.Between(0, 3);
                 let newTrain, xPos, yPos, direction;
                 let xAdd = 0;
                 let yAdd = 0;
                 let foundTrain = true;
+                let trainColor = this.trainColors[Phaser.Math.Between(0, 2)];
                 let rotation;
                 do {
                     dir = Phaser.Math.Between(0, 3);
@@ -118,7 +169,7 @@ class Play extends Phaser.Scene {
                     }
                     else if(dir == 1){ //From Bottom
                         xPos = this.pixelSize * Phaser.Math.Between(0, this.gridXSize - 1) + borderPadding + 50;
-                        yPos = this.pixelSize * this.gridXSize + borderPadding + 50;
+                        yPos = this.pixelSize * this.gridYSize + borderPadding + 50;
                         direction = {x: 0, y: -1};
                         xAdd = 0;
                         yAdd = this.pixelSize * 1;
@@ -143,56 +194,73 @@ class Play extends Phaser.Scene {
                     for(let currentTrain of trainArr){
                         if(dir == 0){ //from top check against bottom
                             if(xPos == currentTrain.x && direction.y == currentTrain.direction.y){
-                                console.log("invalid train top");
                                 foundTrain = false;
                                 break;
                             }
                         } else if(dir == 1) { //from bottom check against top
                             if(xPos == currentTrain.x && direction.y == currentTrain.direction.y){
-                                console.log("invalid train bottom");
                                 foundTrain = false;
                                 break;
                             }
                         } else if(dir == 2){ //from left check against right
                             if(yPos == currentTrain.y && direction.x == currentTrain.direction.x){
-                                console.log("invalid train left");
                                 foundTrain = false;
                                 break;
                             }
                         } else { //from right check against left
                             if(yPos == currentTrain.y && direction.x == currentTrain.direction.x){
-                                console.log("invalid train right");
+                                console.log(xPos);
+                                console.log(yPos);
                                 foundTrain = false;
                                 break;
                             }
                         }
                     }
-                    console.log("bottom of Do-while");
                 } while (!foundTrain);
-                let trainColor = this.trainColors[Phaser.Math.Between(0, 2)];
-                newTrain = new Train(this, xPos, yPos, trainColor[0], undefined, this.trainSpeed + rand, direction).setOrigin(0.5);
-                newTrain.angle = rotation;
-                let trainSize = Phaser.Math.Between(2, 4);
-                for(let i = 1; i < trainSize; i++) {
-                    let segTrain
-                    if (i < trainSize - 1) {
-                        segTrain = new Train(this, xPos + (xAdd * i), yPos + (yAdd * i), trainColor[1], undefined, this.trainSpeed + rand, direction).setOrigin(0.5);
-                        segTrain.angle = rotation;
-                    }
-                    else {
-                        segTrain = new Train(this, xPos + (xAdd * i), yPos + (yAdd * i), trainColor[0], undefined, this.trainSpeed + rand, direction).setOrigin(0.5);
-                        segTrain.angle = 180 + rotation;
-                    }
-                    this.trains.add(segTrain);
+                let indicator;
+                if(dir == 0){ //From Top
+                    indicator = this.add.sprite(xPos - 50, yPos + 50, trainColor[3]).setOrigin(0, 0);
+                    indicator.anims.play(trainColor[3]);
                 }
+                else if(dir == 1){ //From Bottom
+                    indicator = this.add.sprite(xPos - 50, yPos - 53, trainColor[3]).setOrigin(0, 0);
+                    indicator.anims.play(trainColor[3]);
+                }
+                else if(dir == 2){ //From Left
+                    indicator = this.add.sprite(xPos + 50, yPos - 50, trainColor[2]).setOrigin(0, 0);
+                    indicator.anims.play(trainColor[2]);
+                }
+                else{ //From Right
+                    indicator = this.add.sprite(xPos - 53, yPos - 50, trainColor[2]).setOrigin(0, 0);
+                    indicator.anims.play(trainColor[2]);
+                }
+                this.indicateQueue.enqueue(indicator);
+                this.time.delayedCall(2000, () => {
+                    this.indicateQueue.dequeue().destroy();
+                    newTrain = new Train(this, xPos, yPos, trainColor[0], undefined, this.trainSpeed + rand, direction).setOrigin(0.5);
+                    newTrain.angle = rotation;
+                    let trainSize = Phaser.Math.Between(2, 4);
+                    for(let i = 1; i < trainSize; i++) {
+                        let segTrain
+                        if (i < trainSize - 1) {
+                            segTrain = new Train(this, xPos + (xAdd * i), yPos + (yAdd * i), trainColor[1], undefined, this.trainSpeed + rand, direction).setOrigin(0.5);
+                            segTrain.angle = rotation;
+                        }
+                        else {
+                            segTrain = new Train(this, xPos + (xAdd * i), yPos + (yAdd * i), trainColor[0], undefined, this.trainSpeed + rand, direction).setOrigin(0.5);
+                            segTrain.angle = 180 + rotation;
+                        }
+                        this.trains.add(segTrain);
+                    }
                 
-                this.trains.add(newTrain);
+                    this.trains.add(newTrain);
+                });
                 this.currentTime = 0;
-                if(this.intervalTIme > 30){
-                    this.intervalTIme -= 5;
+                if(this.intervalTime > 30){
+                    this.intervalTime -= 1;
                 }
                 if(this.trainSpeed < 5){
-                    this.trainSpeed += 0.1;
+                    this.trainSpeed += 0.02;
                 }
             }
 
@@ -229,7 +297,7 @@ class Play extends Phaser.Scene {
             this.player.x + this.player.width - 25 > coin.x &&
             this.player.y < coin.y + coin.height - 25 &&
             this.player.height - 25 + this.player.y > coin.y) {
-                this.time += coin.scoreValue;
+                this.timer += coin.scoreValue;
                 coin.x = Phaser.Math.Between(1,7) * 100;
                 coin.y = Phaser.Math.Between(1,5) * 100;
                 coin.animation.x = coin.x;
